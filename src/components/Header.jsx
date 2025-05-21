8e4788c2bba75be32d0a69e0407a813d8db0c3b3
Emkay133 <kaymukona133@gmail.com>
2025-05-21 17:50:19 +0200

    updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Menu, ShoppingCart } from "lucide-react";
 
 const Header = () => {
   return (
-      <header>
-          {/* Logo */}
-          <a href="#">
-              
+    <header className="flex py-4 px-10 bg-white h-20">
+      <div>
+        {/* Logo */}
+        <div>
+          <a href="#" className="flex items-center space-x-2">
+            <ShoppingCart size={35} />
+            <p className="text-2xl font-bold">
+              Swap<span className="text-sky-400">Meet</span>
+            </p>
           </a>
-    </header>
-  )
-}
+        </div>
 
-export default Header
+        <div>
+          <Menu />
+          <p>Category</p>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
