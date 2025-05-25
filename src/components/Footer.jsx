6d96ebca34d75ebe580dfdffb35ccedcb2d6a473
Emkay133 <kaymukona133@gmail.com>
2025-05-25 20:27:08 +0200

    home-page done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+import { ShoppingCart } from "lucide-react";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaFacebook } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+
+const Footer = () => {
+  return (
+    <footer className="grid mx-10 mt-19 p-10 border-t-1 border-gray-300">
+
+      {/* Upper Nav */}
+      <div className="grid grid-cols-5">
+        {/* Logo */}
+        <div>
+          <div>
+            <a href="#" className="flex items-center space-x-2">
+              <ShoppingCart size={35} />
+              <p className="text-2xl font-bold">
+                Swap<span className="text-sky-400">Meet</span>
+              </p>
+            </a>
+          </div>
+        </div>
+
+        {/* Buyer */}
+        <div>
+          <h3 className="text-xl mb-5 font-semibold">Buy</h3>
+          <ul>
+            <li className="hover:underline text-md cursor-pointer">
+              Registration
+            </li>
+            <li className="hover:underline text-md cursor-pointer">Auction</li>
+            <li className="hover:underline text-md cursor-pointer">
+              Gift Cards
+            </li>
+            <li className="hover:underline text-md cursor-pointer">Stores</li>
+          </ul>
+        </div>
+
+        {/* Seller */}
+        <div>
+          <h3 className="text-xl mb-5 font-semibold">Sell</h3>
+          <ul>
+            <li className="hover:underline text-md cursor-pointer">
+              Start selling
+            </li>
+            <li className="hover:underline text-md cursor-pointer">
+              How to sell
+            </li>
+            <li className="hover:underline text-md cursor-pointer">
+              Whole sellers
+            </li>
+            <li className="hover:underline text-md cursor-pointer">
+              Affiliates
+            </li>
+          </ul>
+        </div>
+
+        {/* Help */}
+        <div>
+          <h3 className="text-xl mb-5 font-semibold">Help</h3>
+          <ul>
+            <li className="hover:underline text-md cursor-pointer">
+              Seller Center
+            </li>
+            <li className="hover:underline text-md cursor-pointer">
+              Contact Us
+            </li>
+            <li className="hover:underline text-md cursor-pointer">Returns</li>
+            <li className="hover:underline text-md cursor-pointer">Escrow</li>
+          </ul>
+        </div>
+
+        {/* Socials */}
+        <div>
+          <h3 className="text-xl mb-5 font-semibold">Stay Connected</h3>
+          <ul>
+            <li className="flex gap-1 items-center">
+              <a className="hover:underline text-md" href="#">
+                Instagram
+              </a>
+              <AiFillInstagram />
+            </li>
+            <li className="flex gap-2 items-center">
+              <a className="hover:underline text-md" href="#">
+                Facebook
+              </a>
+              <FaFacebook />
+            </li>
+            <li className="flex gap-2 items-center">
+              <a className="hover:underline text-md" href="#">
+                X (Twitter)
+              </a>
+              <FaSquareXTwitter />
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {/* Lower Nav */}
+      <div className="text-center mt-8 cursor-default">
+        SwapMeet &copy; 2025 | All Rights Reserved. 
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import PopularCategories from "../components/PopularCategories";
 import AuctionCard from "../components/AuctionCard";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Hero />
       <PopularCategories />
       <AuctionCard />
+      <Footer />
     </>
   );
 };
