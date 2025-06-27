a02393e9ca4e5a7744fe4e27244e4f13dffeb3b6
Emkay133 <kaymukona133@gmail.com>
2025-06-27 11:11:47 +0200

    url backend again

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -36,7 +36,7 @@ export const CartProvider = ({ children }) => {
     // Send to Backend
     try {
       const res = await fetch(
-        `${"https://d202e0cc-9257-4a32-8c3b-d99083c7ea0b-00-24ny4n04qaxdg.janeway.replit.dev"}/add-to-cart.php`,
+        `${"https://swapmeet-backend.infinityfreeapp.com/swapmeet-backend/"}/add-to-cart.php`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
@@ -61,7 +61,7 @@ export const CartProvider = ({ children }) => {
 
     try {
       await fetch(
-        `${"https://d202e0cc-9257-4a32-8c3b-d99083c7ea0b-00-24ny4n04qaxdg.janeway.replit.dev"}/remove-from-cart.php`,
+        `${"https://swapmeet-backend.infinityfreeapp.com/swapmeet-backend/"}/remove-from-cart.php`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
@@ -81,7 +81,7 @@ export const CartProvider = ({ children }) => {
 
     try {
       await fetch(
-        `${"https://d202e0cc-9257-4a32-8c3b-d99083c7ea0b-00-24ny4n04qaxdg.janeway.replit.dev"}/clear-cart.php`,
+        `${"https://swapmeet-backend.infinityfreeapp.com/swapmeet-backend/"}/clear-cart.php`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
--- src/pages/user/seller/PostItem.jsx
+++ src/pages/user/seller/PostItem.jsx
@@ -49,7 +49,7 @@ const PostItem = () => {
     const fetchCategories = async () => {
       try {
         const res = await fetch(
-          `${"https://d202e0cc-9257-4a32-8c3b-d99083c7ea0b-00-24ny4n04qaxdg.janeway.replit.dev"}/get-categories.php`
+          `${"https://swapmeet-backend.infinityfreeapp.com/swapmeet-backend/"}/get-categories.php`
         );
         const data = await res.json();
 
@@ -171,7 +171,7 @@ const PostItem = () => {
     setIsLoading(true);
 
     const res = await fetch(
-      `${"https://d202e0cc-9257-4a32-8c3b-d99083c7ea0b-00-24ny4n04qaxdg.janeway.replit.dev"}/post-item.php`,
+      `${"https://swapmeet-backend.infinityfreeapp.com/swapmeet-backend/"}/post-item.php`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
